8a463d7f9d289cf7a1099bef3b20846fafcdb2d1
Samuel Thibault <samuel.thibault@polymtl.ca>
2024-09-11 20:28:01 -0400

    Avancé de l'Ex2 - Pas complété

--- ex2.py
+++ ex2.py
@@ -1,4 +1,15 @@
 # TODO: Créer un script pour calculer les ressources nécessaires pour assainir la Seine.
 # TODO: Importer les modules nécessaires.
 
-water_quantity = ...
+water_quantity = float(input("Quelle est la quantité d'eau à assainir? : "))
+
+
+    # TODO: Doit changer pour laisser des virgules proportionnel (un chiffre après)
+
+
+#Qte de "" pour 1L eau + 1 si pas un multiple de 5
+nbFiltre = int(1 / 5 * water_quantity) + (water_quantity % 5 > 0)
+nbLampesUV = int(3 / 5 * water_quantity) + (water_quantity % 5 > 0)
+qteChlore = round(0.5 / 5 * water_quantity, 1)
+
+print(nbFiltre, nbLampesUV, qteChlore)

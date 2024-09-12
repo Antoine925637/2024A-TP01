7dbe8eafdbb9a18336c116ac3579d4c83599ae54
Antoine925637 <antoinegrondin2005@gmail.com>
2024-09-12 19:02:13 -0400

    ex 1 passe le test

--- ex1.py
+++ ex1.py
@@ -1,5 +1,15 @@
 # TODO: Créer un script permettant le formattage du livre des records des JO.
 
+#Collecte des données
 country = input("De quelle nationalité est l'athlète ? ")
-athlete = ...
-date = ...
+athlete = input("Quel est son nom ? ")
+date = input("Date du record ? ")
+sport = input("Dans quelle discipline ? ")
+category = input("Dans une catégorie spécifique ? ")
+record = input("Quel est le record ? ")
+
+#Afficher les données
+print("\nNouveau Record:")
+print("--------------------")
+print(date,"-", sport,"-", category+":")
+print("\t"+athlete,"("+country+")","-",record)
